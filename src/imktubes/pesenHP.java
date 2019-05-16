f38bb6fb32260310f903f40dd74904a19a8fe0c3
SISTEM <SISTEM@172.16.97.70>
2019-05-16 15:57:55 +0700

    Stock n Struck

--- src/imktubes/pesenHP.java
+++ src/imktubes/pesenHP.java
@@ -27,7 +27,11 @@ public class pesenHP extends javax.swing.JFrame {
     File file= new File(url);
     BufferedReader br;
     BufferedWriter write;
-   
+    
+    int beli,harga,totalH;
+    
+  
+    
     DefaultTableModel model;
     public static Vector temp;
 
@@ -66,12 +70,12 @@ public class pesenHP extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         jTable2 = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
+        Tottttttt = new javax.swing.JLabel();
         namaKonser = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         kategori = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        total = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jButton3 = new javax.swing.JButton();
@@ -80,6 +84,8 @@ public class pesenHP extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         jButton6 = new javax.swing.JButton();
         jumlah = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        hg = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("karcis.com");
@@ -113,55 +119,70 @@ public class pesenHP extends javax.swing.JFrame {
         jPanel1.add(jLabel1);
         jLabel1.setBounds(92, 288, 159, 33);
 
+        Tottttttt.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        Tottttttt.setText("0");
+        jPanel1.add(Tottttttt);
+        Tottttttt.setBounds(260, 500, 110, 30);
+
+        namaKonser.setEditable(false);
         namaKonser.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 namaKonserActionPerformed(evt);
             }
         });
         jPanel1.add(namaKonser);
-        namaKonser.setBounds(255, 294, 293, 33);
+        namaKonser.setBounds(260, 290, 293, 33);
 
         jLabel2.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jLabel2.setText("Kategori Tiket:");
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(92, 342, 159, 29);
+        jLabel2.setBounds(80, 370, 159, 29);
 
+        kategori.setEditable(false);
         kategori.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 kategoriActionPerformed(evt);
             }
         });
         jPanel1.add(kategori);
-        kategori.setBounds(255, 338, 280, 33);
+        kategori.setBounds(260, 370, 280, 33);
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jLabel3.setText("Jumlah Tiket:");
         jPanel1.add(jLabel3);
-        jLabel3.setBounds(92, 389, 159, 29);
+        jLabel3.setBounds(80, 460, 159, 29);
 
         jLabel4.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jLabel4.setText("Total Harga: ");
         jPanel1.add(jLabel4);
-        jLabel4.setBounds(92, 430, 159, 32);
-        jPanel1.add(total);
-        total.setBounds(255, 436, 280, 32);
+        jLabel4.setBounds(80, 500, 159, 32);
 
-        jButton1.setText("OK");
+        jButton1.setText("Konfirmasi");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
             }
         });
         jPanel1.add(jButton1);
-        jButton1.setBounds(92, 496, 102, 29);
+        jButton1.setBounds(580, 370, 130, 130);
 
         jButton2.setText("CANCEL");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         jPanel1.add(jButton2);
-        jButton2.setBounds(255, 496, 91, 29);
+        jButton2.setBounds(400, 550, 91, 29);
 
         jButton3.setText("BUKTI PEMBELIAN");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
         jPanel1.add(jButton3);
-        jButton3.setBounds(429, 496, 167, 29);
+        jButton3.setBounds(160, 550, 167, 29);
 
         jButton4.setText("Tambah");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
@@ -170,7 +191,7 @@ public class pesenHP extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButton4);
-        jButton4.setBounds(648, 275, 91, 29);
+        jButton4.setBounds(870, 180, 91, 29);
 
         jButton5.setText("Delete");
         jButton5.addActionListener(new java.awt.event.ActionListener() {
@@ -179,7 +200,7 @@ public class pesenHP extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButton5);
-        jButton5.setBounds(866, 218, 92, 29);
+        jButton5.setBounds(870, 130, 92, 29);
 
         jLabel5.setFont(new java.awt.Font("Square721 Cn BT", 0, 65)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(255, 255, 255));
@@ -194,12 +215,37 @@ public class pesenHP extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButton6);
-        jButton6.setBounds(725, 275, 163, 29);
+        jButton6.setBounds(870, 230, 163, 29);
+
+        jumlah.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jumlahActionPerformed(evt);
+            }
+        });
+        jumlah.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyReleased(java.awt.event.KeyEvent evt) {
+                jumlahKeyReleased(evt);
+            }
+        });
         jPanel1.add(jumlah);
-        jumlah.setBounds(260, 390, 130, 26);
+        jumlah.setBounds(260, 460, 130, 26);
+
+        jLabel6.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
+        jLabel6.setText("Harga 1 Tiket: ");
+        jPanel1.add(jLabel6);
+        jLabel6.setBounds(80, 420, 170, 30);
+
+        hg.setEditable(false);
+        hg.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                hgActionPerformed(evt);
+            }
+        });
+        jPanel1.add(hg);
+        hg.setBounds(260, 420, 280, 26);
 
         getContentPane().add(jPanel1);
-        jPanel1.setBounds(0, 0, 1050, 630);
+        jPanel1.setBounds(0, 0, 1050, 690);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -254,11 +300,8 @@ public class pesenHP extends javax.swing.JFrame {
         int row = jTable2.getSelectedRow();
         namaKonser.setText(jTable2.getValueAt(row, 0).toString());
         kategori.setText(jTable2.getValueAt(row, 1).toString());
+        hg.setText(jTable2.getValueAt(row, 2).toString());
     }//GEN-LAST:event_jTable2MouseClicked
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         // TODO add your handling code here:
@@ -275,6 +318,66 @@ public class pesenHP extends javax.swing.JFrame {
     
 
     }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jumlahActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jumlahActionPerformed
+        // TODO add your handling code here:
+                String a, b;
+        a=jumlah.getText();
+        beli = Integer.parseInt(a);
+        b = hg.getText();
+        harga = Integer.parseInt(b);
+        totalH = harga * beli;
+        
+        Tottttttt.setText(String.valueOf(totalH));
+    }//GEN-LAST:event_jumlahActionPerformed
+
+    private void hgActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_hgActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_hgActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        namaKonser.setText("");
+        kategori.setText("");
+        hg.setText("");
+        
+        
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jumlahKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jumlahKeyReleased
+        // TODO add your handling code here:
+       
+            int a, b, c;
+
+            a = Integer.valueOf(jumlah.getText());
+            b = Integer.valueOf(hg.getText());
+
+            c = a * b;
+            Tottttttt.setText("" + c);
+        
+
+
+    }//GEN-LAST:event_jumlahKeyReleased
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        
+        int selectedr= jTable2.getSelectedRow();
+        String s =  jTable2.getValueAt(selectedr, 3).toString();
+        int stok = Integer.parseInt(s);
+        String jml = jumlah.getText();
+        
+        int stokBaru = (stok - Integer.parseInt(jml));
+        
+        jTable2.getModel().setValueAt(stokBaru,selectedr,3);
+        System.out.println(stokBaru);
+        
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        bukti.setText("Nama Konser: "+namaKonser.getText());
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -312,6 +415,8 @@ public class pesenHP extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Tottttttt;
+    private javax.swing.JTextField hg;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
@@ -323,12 +428,12 @@ public class pesenHP extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane2;
     public javax.swing.JTable jTable2;
     private javax.swing.JTextField jumlah;
     private javax.swing.JTextField kategori;
     private javax.swing.JTextField namaKonser;
-    private javax.swing.JTextField total;
     // End of variables declaration//GEN-END:variables
 }
