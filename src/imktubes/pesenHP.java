0cb742d441c4b9550a6fff0ccd566856380d176f
SISTEM <SISTEM@172.16.97.70>
2019-05-16 15:58:50 +0700

    Stock n Struck

--- src/imktubes/pesenHP.java
+++ src/imktubes/pesenHP.java
@@ -376,7 +376,7 @@ public class pesenHP extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        bukti.setText("Nama Konser: "+namaKonser.getText());
+       
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
